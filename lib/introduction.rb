ad707e6d8bb115005a7fc5f8347ffd3ab3fa592d
Lauren Frankovich <frankovister@gmail.com>
2020-05-21 17:47:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
@@ -14,6 +15,9 @@ end
 introduction_with_language("Dan", "Ember.js")
 
 
+def introduction_with_language_optional(name, language=)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
